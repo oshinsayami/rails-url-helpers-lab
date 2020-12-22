95748fc923756864dd50f761db679dbdad4256a8
Simran <mdrsimran@gmail.com>
2020-12-22 09:34:50 -0600

    commit tree 895a6c50fde0297bf8c3a9b57c574fd1ab0f872c

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,16 @@
+
+
 class Student < ActiveRecord::Base
+after_initialize :student_active
+
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def student_active
+    self.active = false if self.active == nil
+  end
+
+  
 end
